e10eacde4c8bd265a2650bc398dcb76ab4483832
mismaillone <warlockmachine34@gmail.com>
2024-03-15 06:13:05 -0400

    Assignment Completed

---
+++ Main.java
@@ -0,0 +1,15 @@
+package com.driver;
+
+public class Main {
+  public static void main(String[] args) {
+    RWOnly obj = new RWOnly();
+
+    // The field RWOnly.name is not visible
+    
+   obj.setName("Warlock");
+
+   System.out.println(obj.getName());
+
+
+  }
+}
---
+++ RWOnly.java
@@ -0,0 +1,14 @@
+package com.driver;
+
+public class RWOnly {
+    private String name;
+
+    // getter
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
