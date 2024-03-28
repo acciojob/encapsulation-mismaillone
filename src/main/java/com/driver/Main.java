f62ebd898728225771835d35079dcf607252035f
Mohammad Ismail <101501734+mismaillone@users.noreply.github.com>
2024-03-28 18:40:52 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,14 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String[] args) {
+    RWOnly obj = new RWOnly();
+
+    // The field RWOnly.name is not visible
+
+    obj.setName("name");
+
+    System.out.println(obj.getName());
+
+  }
 }
